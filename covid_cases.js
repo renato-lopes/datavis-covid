0eed86e1a48076a2542a84fd0b74e48a86a83ddb
Renato <renato.junior@dcc.ufmg.br>
2021-07-12 15:30:54 -0300

    Add more graphs

--- covid_cases.js
+++ covid_cases.js
@@ -1,49 +1,20 @@
-var state = "MG";
-
-function plot_vaccinations() {
+function plot_vaccinations(state) {
   // set the dimensions and margins of the graph
-  var margin = {top: 10, right: 30, bottom: 30, left: 60},
+  var margin = {top: 30, right: 60, bottom: 30, left: 60},
       width = 1000 - margin.left - margin.right,
       height = 400 - margin.top - margin.bottom;
 
   // append the svg object to the body of the page
-  var svg = d3.select("#covid_vaccination")
+  var svg = d3.select("#cases-graphs")
+    .append("div")
+      .classed("mb-4", true)
     .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
+      .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
+      // .attr("width", width + margin.left + margin.right)
+      // .attr("height", height + margin.top + margin.bottom)
     .append("g")
       .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")"); 
-
-  d3.csv(`data/covid/states/${state}.csv`, function(d){
-    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.newDeathsRolling }
-    })
-    .then(function (data) {
-      console.log(data);
-      // Add X axis --> it is a date format
-      var x = d3.scaleTime()
-        .domain(d3.extent(data, function(d) { return d.date; }))
-        .range([ 0, width ]);
-    
-      // Add Y axis
-      var y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) { return +d.value; })])
-        .range([ height, 0 ]);
-      svg.append("g")
-        .call(d3.axisLeft(y));
-    
-      // Add the line
-      svg.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 1.5)
-        .attr("d", d3.line()
-          .x(function(d) { return x(d.date) })
-          .y(function(d) { return y(d.value) })
-        )
-    }
-  )
 
   d3.csv(`data/covid/states/${state}.csv`, function(d){
     d.date = d3.timeParse("%Y-%m-%d")(d.date);
@@ -67,7 +38,7 @@ function plot_vaccinations() {
       .domain([0, 100])
       .range([ height, 0]);
       svg.append("g")
-      .attr("transform", "translate(911,0)")
+      .attr("transform", `translate(${width+1},0)`)
       .call(d3.axisRight(y));
 
       // Bars
@@ -91,8 +62,68 @@ function plot_vaccinations() {
           .attr("height", function(d) { return height - y(d.vaccinated_second_per_100_inhabitants); })
           .attr("fill", "#0c7af0")
       
+      // Title
+      svg.append("text")
+        .attr("x", (width / 2))             
+        .attr("y", -16)
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px") 
+        .style("font-family", "sans-serif")
+        .text(`Effects of Vaccination on Reported Cases in ${state}`);
+      
+      svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0-50)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("text-anchor", "middle")
+        .style("font-size", "14px") 
+        .style("font-family", "sans-serif")
+        .text("New Daily Cases (moving 7-day average)");
+
+      svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", width+36)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("text-anchor", "middle")
+        .style("font-size", "14px") 
+        .style("font-family", "sans-serif")
+        .text("% of population vaccinated");
+      
       }
+  )
+  d3.csv(`data/covid/states/${state}.csv`, function(d){
+    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.newDeathsRolling }
+    })
+    .then(function (data) {
+      console.log(data);
+      // Add X axis --> it is a date format
+      var x = d3.scaleTime()
+        .domain(d3.extent(data, function(d) { return d.date; }))
+        .range([ 0, width ]);
+    
+      // Add Y axis
+      var y = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d) { return +d.value; })])
+        .range([ height, 0 ]);
+      svg.append("g")
+        .call(d3.axisLeft(y));
+    
+      // Add the line
+      svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "red")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+          .x(function(d) { return x(d.date) })
+          .y(function(d) { return y(d.value) })
+        )
+    }
   )
 }
 
-plot_vaccinations()
+plot_vaccinations('MG')
+plot_vaccinations('SP')
+plot_vaccinations('AM')
