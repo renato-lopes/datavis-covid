4816216b898909549afeeb295ed96179309d6e47
Renato <renato.junior@dcc.ufmg.br>
2021-08-17 20:58:59 -0300

    Update csv loading

--- covid_cases.js
+++ covid_cases.js
@@ -16,181 +16,186 @@ function plot_vaccinations(parent, state) {
     .append("g")
       .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")"); 
-
-  d3.csv(`data/covid/states/${state}.csv`, function(d){
-    d.date = d3.timeParse("%Y-%m-%d")(d.date);
-    return d;
+  Promise.all([
+    d3.csv(`data/covid/states/${state}.csv`, function(d){
+      d.date = d3.timeParse("%Y-%m-%d")(d.date);
+      return d;
+    }),
+    d3.csv(`data/covid/states/${state}.csv`, function(d){
+      return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.newCasesRolling }
     })
-    .then(function (data) {
-      // X axis
-      var x = d3.scaleTime()
-        .domain(d3.extent(data, function(d) { return d.date; }))
-        .range([ 0, width ]);
-      svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-      .selectAll("text")
-        // .attr("transform", "translate(-10,0)rotate(-45)")
-        // .style("text-anchor", "end");
+  ]).then(
+    function(files) {
+      plot_vaccines(files[0]);
+      plot_cases(files[1]);
+    }
+  )
+  
+  function plot_vaccines(data) {
+    // X axis
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      // .attr("transform", "translate(-10,0)rotate(-45)")
+      // .style("text-anchor", "end");
 
-      // Add Y axis
-      var y = d3.scaleLinear()
-      .domain([0, 100])
-      .range([ height, 0]);
-      svg.append("g")
-      .attr("transform", `translate(${width+1},0)`)
-      .call(d3.axisRight(y));
+    // Add Y axis
+    var y = d3.scaleLinear()
+    .domain([0, 100])
+    .range([ height, 0]);
+    svg.append("g")
+    .attr("transform", `translate(${width+1},0)`)
+    .call(d3.axisRight(y));
 
-      // Bars
-      svg.selectAll("firstdose")
+    // Bars
+    svg.selectAll("firstdose")
+    .data(data)
+    .enter()
+    .append("rect")
+      .attr("x", function(d) { return x(d.date) - (width/data.length)/2; })
+      .attr("y", function(d) { return y(d.vaccinated_per_100_inhabitants); })
+      .attr("width", width/data.length)
+      .attr("height", function(d) { return height - y(d.vaccinated_per_100_inhabitants); })
+      .attr("fill", "#69b3a2")
+    
+    svg.selectAll("seconddose")
       .data(data)
       .enter()
       .append("rect")
         .attr("x", function(d) { return x(d.date) - (width/data.length)/2; })
-        .attr("y", function(d) { return y(d.vaccinated_per_100_inhabitants); })
+        .attr("y", function(d) { return y(d.vaccinated_second_per_100_inhabitants); })
         .attr("width", width/data.length)
-        .attr("height", function(d) { return height - y(d.vaccinated_per_100_inhabitants); })
-        .attr("fill", "#69b3a2")
-      
-      svg.selectAll("seconddose")
-        .data(data)
-        .enter()
-        .append("rect")
-          .attr("x", function(d) { return x(d.date) - (width/data.length)/2; })
-          .attr("y", function(d) { return y(d.vaccinated_second_per_100_inhabitants); })
-          .attr("width", width/data.length)
-          .attr("height", function(d) { return height - y(d.vaccinated_second_per_100_inhabitants); })
-          .attr("fill", "#0c7af0")
-      
-      // Title
-      // svg.append("text")
-      //   .attr("x", (width / 2))             
-      //   .attr("y", -16)
-      //   .attr("text-anchor", "middle")  
-      //   .style("font-size", "16px") 
-      //   .style("font-family", "sans-serif")
-      //   .text(`Effects of Vaccination on Reported Cases in ${state}`);
-      
-      svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0-margin.right)
-        .attr("x",0 - (height / 2))
-        .attr("dy", "1em")
-        .attr("text-anchor", "middle")
-        .style("font-size", "14px") 
-        .style("font-family", "sans-serif")
-        .text("New Daily Cases");
-
-      svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", width+36)
-        .attr("x",0 - (height / 2))
-        .attr("dy", "1em")
-        .attr("text-anchor", "middle")
-        .style("font-size", "14px") 
-        .style("font-family", "sans-serif")
-        .text("% population vaccinated");
-      
-      }
-  )
-  d3.csv(`data/covid/states/${state}.csv`, function(d){
-    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.newCasesRolling }
-    })
-    .then(function (data) {
-      // Add X axis --> it is a date format
-      var x = d3.scaleTime()
-        .domain(d3.extent(data, function(d) { return d.date; }))
-        .range([ 0, width ]);
+        .attr("height", function(d) { return height - y(d.vaccinated_second_per_100_inhabitants); })
+        .attr("fill", "#0c7af0")
     
-      // Add Y axis
-      var y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) { return +d.value; })])
-        // .domain([0, 300])
-        .range([ height, 0 ]);
-      svg.append("g")
-        .call(d3.axisLeft(y));
+    // Title
+    // svg.append("text")
+    //   .attr("x", (width / 2))             
+    //   .attr("y", -16)
+    //   .attr("text-anchor", "middle")  
+    //   .style("font-size", "16px") 
+    //   .style("font-family", "sans-serif")
+    //   .text(`Effects of Vaccination on Reported Cases in ${state}`);
     
-      // Add the line
-      svg.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "red")
-        .attr("stroke-width", 1.5)
-        .attr("d", d3.line()
-          .x(function(d) { return x(d.date) })
-          .y(function(d) { return y(d.value) })
-        )
-      // This allows to find the closest X index of the mouse:
-      var bisect = d3.bisector(function(d) { return d.date; }).left;
+    svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0-margin.right)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("text-anchor", "middle")
+      .style("font-size", "14px") 
+      .style("font-family", "sans-serif")
+      .text("New Daily Cases");
 
-      // Create the circle that travels along the curve of chart
-      var focus = svg
-        .append('g')
-        .append('circle')
-          .style("fill", "black")
-          .attr("stroke", "black")
-          .attr('r', 3)
-          .style("opacity", 0)
-
-      // Create the text that travels along the curve of chart
-      var focusText1 = svg
-        .append('g')
-        .append('text')
-          .style("opacity", 0)
-          .attr("text-anchor", "middle")
-          .attr("alignment-baseline", "middle")
-      var focusText2 = svg
-        .append('g')
-        .append('text')
-          .style("opacity", 0)
-          .attr("text-anchor", "middle")
-          .attr("alignment-baseline", "middle")
-      // Create a rect on top of the svg area: this rectangle recovers mouse position
-      svg
-        .append('rect')
-        .style("fill", "none")
-        .style("pointer-events", "all")
-        .attr('width', width)
-        .attr('height', height)
-        .on('mouseover', mouseover)
-        .on('mousemove', mousemove)
-        .on('mouseout', mouseout);
-      
-      // What happens when the mouse move -> show the annotations at the right positions.
-      function mouseover() {
-        focus.style("opacity", 1)
-        focusText1.style("opacity",1)
-        focusText2.style("opacity",1)
-      }
-
-      function mousemove(d, i) {
-        // recover coordinate we need
-        var mousex = d3.pointer(d);
-        mousex = mousex[0];
-        var x0 = x.invert(mousex);
-        var i = bisect(data, x0, 1);
-        selectedData = data[i]
-        focus
-          .attr("cx", x(selectedData.date))
-          .attr("cy", y(selectedData.value))
-        focusText1
-          .html(selectedData.date.toLocaleDateString('pt-BR'))
-          .attr("x", d3.max([d3.min([x(selectedData.date), width - 50]), 50]))
-          .attr("y", d3.max([y(selectedData.value)-40, 20]))
-        focusText2
-          .html(Math.ceil(selectedData.value))
-          .attr("x", d3.max([d3.min([x(selectedData.date), width - 50]), 50]))
-          .attr("y", d3.max([y(selectedData.value)-20, 40]))
-      }
-      
-      function mouseout() {
-        focus.style("opacity", 0)
-        focusText1.style("opacity", 0)
-        focusText2.style("opacity", 0)
-      }
-
+    svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", width+36)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("text-anchor", "middle")
+      .style("font-size", "14px") 
+      .style("font-family", "sans-serif")
+      .text("% population vaccinated");
+    
     }
-  )
+  function plot_cases(data) {
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+  
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      // .domain([0, 300])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+  
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "red")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+      )
+    // This allows to find the closest X index of the mouse:
+    var bisect = d3.bisector(function(d) { return d.date; }).left;
+
+    // Create the circle that travels along the curve of chart
+    var focus = svg
+      .append('g')
+      .append('circle')
+        .style("fill", "black")
+        .attr("stroke", "black")
+        .attr('r', 3)
+        .style("opacity", 0)
+
+    // Create the text that travels along the curve of chart
+    var focusText1 = svg
+      .append('g')
+      .append('text')
+        .style("opacity", 0)
+        .attr("text-anchor", "middle")
+        .attr("alignment-baseline", "middle")
+    var focusText2 = svg
+      .append('g')
+      .append('text')
+        .style("opacity", 0)
+        .attr("text-anchor", "middle")
+        .attr("alignment-baseline", "middle")
+    // Create a rect on top of the svg area: this rectangle recovers mouse position
+    svg
+      .append('rect')
+      .style("fill", "none")
+      .style("pointer-events", "all")
+      .attr('width', width)
+      .attr('height', height)
+      .on('mouseover', mouseover)
+      .on('mousemove', mousemove)
+      .on('mouseout', mouseout);
+    
+    // What happens when the mouse move -> show the annotations at the right positions.
+    function mouseover() {
+      focus.style("opacity", 1)
+      focusText1.style("opacity",1)
+      focusText2.style("opacity",1)
+    }
+
+    function mousemove(d, i) {
+      // recover coordinate we need
+      var mousex = d3.pointer(d);
+      mousex = mousex[0];
+      var x0 = x.invert(mousex);
+      var i = bisect(data, x0, 1);
+      selectedData = data[i]
+      focus
+        .attr("cx", x(selectedData.date))
+        .attr("cy", y(selectedData.value))
+      focusText1
+        .html(selectedData.date.toLocaleDateString('pt-BR'))
+        .attr("x", d3.max([d3.min([x(selectedData.date), width - 50]), 50]))
+        .attr("y", d3.max([y(selectedData.value)-40, 20]))
+      focusText2
+        .html(Math.ceil(selectedData.value))
+        .attr("x", d3.max([d3.min([x(selectedData.date), width - 50]), 50]))
+        .attr("y", d3.max([y(selectedData.value)-20, 40]))
+    }
+    
+    function mouseout() {
+      focus.style("opacity", 0)
+      focusText1.style("opacity", 0)
+      focusText2.style("opacity", 0)
+    }
+
+  }
 }
 
 function process_vaccine_name(name) {
